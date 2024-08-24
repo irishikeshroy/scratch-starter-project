ebd72fbf5ce08cfb5a364ea59484cb31d4f4cf23
irishikeshroy <rishikeshroy43@gmail.com>
2024-08-24 13:41:52 +0530

    css fixxed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   darkMode: "class",
-  purge: ["./src/**/*.js"],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: {
+      safelist: [/^bg-/, /^text-/], // Safelist classes that match the patterns
+    },
+  },
   variants: {},
   plugins: [],
 };
